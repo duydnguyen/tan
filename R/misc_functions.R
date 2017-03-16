aed7c49836caf9ea1a72b9e3019473326bfb434a
Duy Nguyen <gofrog88@gmail.com>
2017-03-15 22:45:39 -0500

    export, and documenting movingAverage()

--- R/misc_functions.R
+++ R/misc_functions.R
@@ -303,11 +303,19 @@ evalPC_sep2 <- function(Pvals, lenT = 100) {
     return(PC)
 }
 
-### Moving Average
-# x: the vector
-# n: the number of samples
-# centered: if FALSE, then average current sample and previous (n-1) samples
+
+
+#' Moving Average
+#'
+#' @param x the vector
+#' @param n the number of samples
+#' @param centered  if FALSE, then average current sample and previous (n-1) samples
 #           if TRUE, then average symmetrically in past and future. (If n is even, use one more sample from future.)
+#'
+#' @return
+#' @export
+#'
+#' @examples
 movingAverage <- function(x, n=1, centered=FALSE) {
 
     if (centered) {
