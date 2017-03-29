2339821b1f061713dc21f1d87e2c799e039fb83e
Duy Nguyen <duynguyen@Duy-Pianist.local>
2017-03-29 16:56:22 -0500

    small changes

--- R/createDesigns.R
+++ R/createDesigns.R
@@ -21,17 +21,4 @@
 
 setMethod("createDesigns", signature("tanDb"), .createDesigns)
 
-## or write function directly to setMethod()
-# setMethod("createDesigns", signature("tanDb"),
-#           function(object, s.size, LHD, Uniform) {
-#             total <- length(object@coverage)
-#             Designs <- matrix(NA, nrow = total, ncol = s.size)
-#             for (site in 1:total) {
-#                 if (site %%1000 == 0) {
-#                     print(paste(site,' out of', total))
-#                 }
-#                 Designs[site, ] <- tan::Sampling(s.size = s.size, start = 1 , end = length(object@coverage[[site]][1,]), LHD = LHD, Uniform = Uniform)
-#             }
-#             object@Designs <- Designs
-#             object
-# })
+
